29edf6b9ce307ce868f621c0be3e9678b5cf8389
mikysunnytao <813172732@qq.com>
2023-09-07 01:42:38 +0800

    []

--- openim-server/main/scripts/install/environment.sh
+++ openim-server/main/scripts/install/environment.sh
@@ -91,12 +91,24 @@ def "ZOOKEEPER_ADDRESS" "127.0.0.1"      # Zookeeper的地址
 def "ZOOKEEPER_USERNAME" ""              # Zookeeper的用户名
 def "ZOOKEEPER_PASSWORD" ""              # Zookeeper的密码
 
+# ###################### MySQL 配置信息 ######################
+# def "MYSQL_PORT" "13306"                 # MySQL的端口
+# def "MYSQL_ADDRESS" "127.0.0.1"          # MySQL的地址
+# def "MYSQL_USERNAME" "${USER}"           # MySQL的用户名
+# def "MYSQL_PASSWORD" "${PASSWORD}"       # MySQL的密码
+# def "MYSQL_DATABASE" "${DATABASE_NAME}"  # MySQL的数据库名
+# def "MYSQL_MAX_OPEN_CONN" "1000"         # 最大打开的连接数
+# def "MYSQL_MAX_IDLE_CONN" "100"          # 最大空闲连接数
+# def "MYSQL_MAX_LIFETIME" "60"            # 连接可以重用的最大生命周期（秒）
+# def "MYSQL_LOG_LEVEL" "4"                # 日志级别
+# def "MYSQL_SLOW_THRESHOLD" "500"         # 慢查询阈值（毫秒）
+
 ###################### MySQL 配置信息 ######################
-def "MYSQL_PORT" "13306"                 # MySQL的端口
+def "MYSQL_PORT" "3306"                 # MySQL的端口
 def "MYSQL_ADDRESS" "127.0.0.1"          # MySQL的地址
-def "MYSQL_USERNAME" "${USER}"           # MySQL的用户名
-def "MYSQL_PASSWORD" "${PASSWORD}"       # MySQL的密码
-def "MYSQL_DATABASE" "${DATABASE_NAME}"  # MySQL的数据库名
+def "MYSQL_USERNAME" "opemim"           # MySQL的用户名
+def "MYSQL_PASSWORD" "85dfFBP7mG6ETJXY"       # MySQL的密码
+def "MYSQL_DATABASE" "opemim"  # MySQL的数据库名
 def "MYSQL_MAX_OPEN_CONN" "1000"         # 最大打开的连接数
 def "MYSQL_MAX_IDLE_CONN" "100"          # 最大空闲连接数
 def "MYSQL_MAX_LIFETIME" "60"            # 连接可以重用的最大生命周期（秒）
